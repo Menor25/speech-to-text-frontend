ea805a4e347200955101b830963b2c038ff901cd
Menor25 <ajirimenor@gmail.com>
2025-02-12 13:16:43 +0100

    change api url

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -3,7 +3,7 @@ import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow
 import DeleteIcon from "@mui/icons-material/Delete";
 import axios from "axios";
 
-const BACKEND_URL = "http://127.0.0.1:5000";
+const BACKEND_URL = "https://speech-to-text-server-jos6.onrender.com";
 
 const Dashboard = () => {
   const [reviews, setReviews] = useState([]);
